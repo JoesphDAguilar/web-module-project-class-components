95bc7752f58f4ec02a640de22853a13a4531b683
JoesphAguilar <josephjd93@gmail.com>
2022-04-12 11:37:44 -0700

    Starting project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
+
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      task: this.task,
+      id: this.id,
+      completed: false
+    }
+  }
   render() {
     return (
       <div>
